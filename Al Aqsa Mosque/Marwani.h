b9c7e6f5afe9b4a842e2d56b4e8b3ed58bada8dd
SalahMd <123739157+SalahMd@users.noreply.github.com>
2024-01-05 23:49:08 +0300

    Update Marwani.h

--- Al Aqsa Mosque/Marwani.h
+++ Al Aqsa Mosque/Marwani.h
@@ -6,7 +6,7 @@
 #include "Point.h"
 class Marwani {
 public: 
-	void drawMarwaniMosque(Point begin,float lenght,float width,float height,float depth,int wallTexrure,int carpet,int marble,int* texturess,int walls,int marwaniWall){
+	void drawMarwaniMosque(Point begin,float lenght,float width,float height,float depth,int wallTexrure,int carpet,int marble,int* texturess,int walls,int marwaniWall ,int blackMetal){
 		primitives p1;
 		//draw outside walls
 		//back
@@ -43,7 +43,7 @@ public:
 		
 		//draw pillars inside mosque and wall above them
 	 Pillar mosquePillar(2,height/2.6-(0.02*height/2.6));
-	 for(int j = depth+10;j<=width-10-depth;j+=40){
+	 for(int j = depth+20;j<=width-10-depth;j+=40){
 		 for (int i =depth+20 ;i<lenght-depth-10;i+=40){	 
 				glPushMatrix();
 				glTranslated(begin.x+j,begin.y+height/2.1-(0.02*height/2.1),begin.z+i);
@@ -52,12 +52,23 @@ public:
 				p1.Arch1(20,22,5,16,texturess);
 				glPopMatrix();
 			 mosquePillar.cube_cylinder_pillar(Point(begin.x+j-0.5,begin.y, begin.z+i+19),marble,marble);
-			 
+	 }}
+	 for (int i = 5;i<height-10;i+=5){
+		 p1.DrawWall(Point(begin.x+30,begin.y+i,begin.z+depth),32,1,8,blackMetal);
 	 }
+	 for (int i = 5;i<height-10;i+=5){
+		 p1.DrawWall(Point(begin.x+70,begin.y+i,begin.z+depth),32,1,8,blackMetal);
 	 }
-
+	 float space = 30;
+	 for (int i = 0;i<4;i++){
+		 if(i==3)
+			 space = 100;
+		 p1.DrawWall(Point(begin.x+space,begin.y,begin.z+depth),2,30,8,blackMetal);
+		 if(i==1)
+			 space+=10;
+		 if(i==0)
+		 space +=30;
+		 
+	 }
 		};
-
-
-
 };
